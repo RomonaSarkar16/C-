f48f9a41029a68653278449025c0152551e2eb4f
RomonaSarkar16 <85759553+RomonaSarkar16@users.noreply.github.com>
2021-09-24 21:10:43 +0600

    Add files via upload

---
+++ Input/Input/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Input
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your Name : ");
+            string username = Console.ReadLine();
+            Console.WriteLine("Your Name : " + username);
+            Console.ReadLine();
+
+            Console.WriteLine("enter your age : ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your age : " + age);
+            Console.ReadLine();
+
+        }
+    }
+}
