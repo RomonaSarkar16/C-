85787a6c562357f806679afa57dc6c0162f9e906
RomonaSarkar16 <85759553+RomonaSarkar16@users.noreply.github.com>
2021-09-24 21:10:11 +0600

    Add files via upload

---
+++ LabTask1/LabTask1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace LabTask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Name : Romona Magdalene Sarkar");
+            Console.WriteLine("Student ID: 20-42186-1");
+           
+
+            char C = 'A';
+            int myC = C;
+            Console.WriteLine("_____Implicit Casting_____");
+            Console.WriteLine("Character : " + C);
+            Console.WriteLine("Character to Integer : "+myC);
+
+            double myD = 12.22;
+            int myInt = (int) myD;
+
+            Console.WriteLine("______Explicit Casting______");
+            Console.WriteLine("Double number : "+myD);
+            Console.WriteLine("Double to Integer : "+myInt);
+            Console.ReadKey();
+
+
+
+
+
+        }
+    }
+}
