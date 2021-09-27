06b66d9c05672673c2ce7ba03f0b659f92998985
RomonaSarkar16 <85759553+RomonaSarkar16@users.noreply.github.com>
2021-09-27 22:26:40 +0600

    Add files via upload

---
+++ String/String/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+
+namespace String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            Console.Write(" OH ! Darling What's your Name : ");
+            string inp= Console.ReadLine();
+            Console.WriteLine("My name is " +  inp);
+            Console.WriteLine("whats the year ? ");
+            string age = Console.ReadLine();
+            Console.WriteLine("yess! And This January I will be " + (Convert.ToInt32(age) - 2000));
+
+        }
+    }
+}
