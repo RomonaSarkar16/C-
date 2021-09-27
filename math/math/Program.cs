5e08e80bdf52303c800b7bef400bb945969d5673
RomonaSarkar16 <85759553+RomonaSarkar16@users.noreply.github.com>
2021-09-27 22:22:17 +0600

    Add files via upload

---
+++ math/math/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace math
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Maximum Number  :" + Math.Max(2,23));
+            Console.WriteLine("Manimum Number  :" + Math.Min(2, 23));
+            Console.WriteLine("Round Number    :" + Math.Round(4.9));
+            Console.WriteLine("Square Number   :" + Math.Sqrt(2.4));
+            Console.WriteLine("Positive Number :" + Math.Abs(-34));
+        }
+    }
+}
