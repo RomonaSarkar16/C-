d7280e330468c417203c1d4f99cc02a8fad37002
RomonaSarkar16 <85759553+RomonaSarkar16@users.noreply.github.com>
2021-09-28 17:36:45 +0600

    Add files via upload

---
+++ Lab Task 2/Lab Task 2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Lab_Task_2
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---------------");
+            Console.WriteLine("----Access Modifier and Empty Constructors----");
+            Romona r = new Romona(); //1st object for empty constructor
+
+            r.name = "Romona Magdalene Sarkar";
+            r.id = "20-42186-1";
+            r.setCgpa(3.86);
+            r.semester=6;
+            r.setProgram("CSE");
+            r.university = "AIUB";
+            r.Display();
+            Console.WriteLine("---------------");
+            
+            //Creating 2nd Object for parameterized constructor
+            Console.WriteLine("----Access Modifier and other Constructor----");
+            Romona r2 = new Romona("Romona Magdalene","20-42186-1",3.86,6,"CSE","AIUB");
+            r2.Display();
+
+            //Creating 3rd Object for Copy Constructor
+            Console.WriteLine("____COPY CONSTRUCTOR CALLED____");
+            Romona r3 = new Romona(r2);
+           
+
+           
+            r3.Display();
+            
+
+
+        }
+        
+    }
+}
---
+++ Lab Task 2/Lab Task 2/Romona.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_Task_2
+{
+    class Romona
+    {
+        public string name;
+        internal string id;
+        private double cgpa;
+        public int semester;
+        protected string program;
+        public string university;
+
+        public void setName(string name)
+        {
+            this.name = name;
+        }
+        public void setId(string id)
+        {
+            this.id = id;
+        }
+        public void setCgpa(double cgpa)
+        {
+            this.cgpa = cgpa;
+        }
+        public void setSemester(int semester)
+        {
+            this.semester = semester;
+        }
+        public void setProgram(string program)
+        {
+            this.program = program;
+        }
+        public void setUniversity(string university)
+        {
+            this.university = university;
+        }
+        public double getCgpa()
+        {
+            return cgpa;
+        }
+        //Empty Constructor 
+        public Romona()
+        {
+            Console.WriteLine("______EMPTY CONSTRUCTOR CALLED______");
+
+        }
+        //parameterized Constructor 
+        public Romona(string name,string id,double cgpa,int semester,string program,string university)
+        {
+            Console.WriteLine("_______PARAMETERIZED CONSTRUCTOR CALLED_________");
+            this.name = name;
+            this.id = id;
+            this.cgpa = cgpa;
+            this.semester = semester;
+            this.program = program;
+            this.university = university;
+
+   
+        
+        }
+        public Romona(double cgpa)
+        {
+            this.cgpa = cgpa;
+        }
+
+        //Copy Constructor using
+
+        public Romona(Romona  R)
+
+        {
+            name = R.name;
+            id = R.id;
+            cgpa = R.cgpa;
+            semester = R.semester;
+            program = R.program;
+            university = R.university;
+        }
+
+        //static constructor 
+        static Romona()
+        {
+            Console.WriteLine("____STATIC CONSTRUCTOR CALLED____");
+        }
+
+        /*private Romona()
+        {
+            Console.WriteLine("Private Constructor");
+        }*/
+
+
+
+        public void Display()
+        {
+            Console.WriteLine("Name      : " + name);
+            Console.WriteLine("ID        : "+ id);
+            Console.WriteLine("Cgpa      : " + cgpa);
+            Console.WriteLine("Semester  : " + semester);
+            Console.WriteLine("Program   : "+ program);
+            Console.WriteLine("University: "+ university);
+        }
+
+    }
+}
