9eda1139703b031b3f12f03cc4b4dcce1cd7fa2a
RomonaSarkar16 <85759553+RomonaSarkar16@users.noreply.github.com>
2021-09-28 17:38:22 +0600

    Add files via upload

---
+++ Enum/Enum/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Enum
+{
+    class Program
+    {
+        enum Days { Sunday, Monday, Tuesday = 12, Wednesday = 21, Thursday }
+        static void Main(string[] args)
+        {
+            Days d = Days.Thursday;
+            int a = (int)Days.Thursday;
+            Console.WriteLine("Day:"+d);
+            Console.WriteLine("location:"+a);
+            
+        }
+    }
+}
