596b5b2f544f6993b57654a0d99654f643bd5e5a
RomonaSarkar16 <85759553+RomonaSarkar16@users.noreply.github.com>
2021-09-28 17:40:04 +0600

    Add files via upload

---
+++ Constructor1/Constructor1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Constructor1
+{
+    class Program
+    {
+        struct Box
+        {
+            public int height;
+            public int width;
+
+            public Box(int H, int W)
+            {
+                Console.WriteLine("Parameterized constructor");
+                height = H;
+                width = W;
+            }
+        
+
+        }
+        static void Main(string[] args)
+        {
+            Box b1 = new Box(200,150); //new instance create
+            Console.WriteLine("Box height:"+b1.height);
+            Console.WriteLine("Box Width : "+b1.width);
+           
+        }
+    }
+}
