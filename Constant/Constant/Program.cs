8af5b06e49295666da7491007b1299b8f29fe225
RomonaSarkar16 <85759553+RomonaSarkar16@users.noreply.github.com>
2021-09-28 17:39:26 +0600

    Add files via upload

---
+++ Constant/Constant/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Constant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string age = Console.ReadLine();
+            Console.WriteLine("Your Age : " + age);
+            const long Nid = 911692345;
+            //Nid = 12083949357;
+            //when we use constant it cannot be updated later!!!
+
+            Console.WriteLine("Nid :" + Nid);
+
+        }
+    }
+}
