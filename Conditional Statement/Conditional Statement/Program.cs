1bc1abb89c1b48a75f122d7d7b830c371be8ceb8
RomonaSarkar16 <85759553+RomonaSarkar16@users.noreply.github.com>
2021-10-09 12:22:38 +0600

    Add files via upload

---
+++ Conditional Statement/Conditional Statement/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Conditional_Statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("___NiD Registration___");
+
+            Console.WriteLine("Enter your Age : ");
+            string age = Console.ReadLine();
+            int Age = Convert.ToInt32(age);
+            
+            if (Age > 17) 
+            {
+                Console.WriteLine("Start Registration for Your NID");
+            }
+            else if (Age == 17)
+            {
+                Console.WriteLine("Need to wait for your Legal age");
+            }
+            else if (Age < 10)
+            {
+                Console.WriteLine("Kid Stay out Of this !");
+            }
+            else
+            {
+                Console.WriteLine("Sorry Not Possible U are not adult .");
+            }
+
+            int time = 22;
+            string result = (time < 18) ? "Good day." : "Good evening.";
+            Console.WriteLine(result);
+
+
+            Console.ReadLine();
+        }
+    }
+}
