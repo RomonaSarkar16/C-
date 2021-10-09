686a260f9943cd7456097cc495142841bc605e3a
RomonaSarkar16 <85759553+RomonaSarkar16@users.noreply.github.com>
2021-10-09 12:24:53 +0600

    Add files via upload

---
+++ Jump Statement 1/Jump Statement 1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Jump_Statement_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           for(int a =1;a<5;a++)
+            {
+                switch(a)
+                {
+                    case 1:
+                        Console.WriteLine("In case 1");
+                        goto case 3;
+                    case 2:
+                        Console.WriteLine("In case 2");
+                        goto case 1;
+                    case 3:
+                        Console.WriteLine("In case 3");
+                        goto default;
+                    default:
+                        Console.WriteLine("In default");
+                        break;
+
+
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
