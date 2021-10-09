587b5c516fb692a85eece7b66d2331970efe57b1
RomonaSarkar16 <85759553+RomonaSarkar16@users.noreply.github.com>
2021-10-09 12:24:19 +0600

    Add files via upload

---
+++ IF Else/IF Else/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace IF_Else
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ava = "Hardin";
+            if (ava == "Hardin")
+            {
+                Console.WriteLine("I Love you , Hardin Scott");
+
+            }
+            else
+                {
+
+                Console.WriteLine("I Hate you , Hardin Scott");
+            }
+        }
+    }
+}
