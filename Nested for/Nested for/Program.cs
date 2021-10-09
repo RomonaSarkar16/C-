b925ab87ba71933fd1d14dd68c8b350f9c7a8080
RomonaSarkar16 <85759553+RomonaSarkar16@users.noreply.github.com>
2021-10-09 12:27:21 +0600

    Add files via upload

---
+++ Nested for/Nested for/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Nested_for
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int a;
+            for (a = 1; a <= 4; a++)
+            {
+                Console.WriteLine("For Loop " + a);
+                for (int k = 0; k <= 2; k++)
+                {
+                    Console.WriteLine("Nested for Loop " + k);
+                }
+            }
+            Console.WriteLine("OUT");
+        }
+    }
+}
