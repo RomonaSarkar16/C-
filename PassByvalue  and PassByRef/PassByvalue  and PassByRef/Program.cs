6b9aaac193ed4c32b7200cc50eaea25f1ddc9ede
RomonaSarkar16 <85759553+RomonaSarkar16@users.noreply.github.com>
2021-10-09 12:28:46 +0600

    Add files via upload

---
+++ PassByvalue  and PassByRef/PassByvalue  and PassByRef/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace PassByvalue__and_PassByRef
+{
+    class Program
+    {
+        public static void add(int a)
+        {
+            a = a + 100;
+            Console.WriteLine("Inside the Method : " + a);
+        }
+        public static void Multiply(ref int b)
+        {
+            b = b * 100;
+            Console.WriteLine("Inside the Multiply method : "+ b);
+        }
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 20;
+           
+            
+            Console.WriteLine("Before using method : " + a);
+            add(20);
+
+            Console.WriteLine("After calling Add method : " + a);
+            Console.WriteLine();
+            Console.WriteLine("Before using method : " + b);
+            Multiply(ref b);
+            Console.WriteLine("After using Ref and calling Multiply  method : " + b);
+
+        }
+    }
+}
