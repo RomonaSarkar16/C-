25c02d786b01bc744f3d0ebfec2e0c471ce4d53b
RomonaSarkar16 <85759553+RomonaSarkar16@users.noreply.github.com>
2021-10-09 12:27:45 +0600

    Add files via upload

---
+++ Nested IF/Nested IF/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Nested_IF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 10;
+
+            if (i == 10)
+            {
+
+                // Nested - if statement
+                // Will only be executed if statement
+                // above it is true
+                if (i < 12)
+                    Console.WriteLine("i is smaller than 12 too");
+                else
+                    Console.WriteLine("i is greater than 15");
+            }
+    }
+}
