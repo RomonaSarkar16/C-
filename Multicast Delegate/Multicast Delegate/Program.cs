0e26544dc7da2792007340e19d040c15a1ebcfe4
RomonaSarkar16 <85759553+RomonaSarkar16@users.noreply.github.com>
2021-11-11 23:21:08 +0600

    Add files via upload

---
+++ Multicast Delegate/Multicast Delegate/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Multicast_Delegate
+{
+    public delegate void MulticastDelegate(int a, int b);
+
+    class Calculation
+    {
+        public void Addition(int a, int b)
+        {
+            Console.WriteLine("Addition Result = " + (a + b));
+        }
+
+        public void Subtraction(int a, int b)
+        {
+            Console.WriteLine("Subtraction Result = " + (a - b));
+        }
+        public void Multiplication(int a, int b)
+        {
+            Console.WriteLine("Multiplication Result = " + (a * b));
+        }
+        public void Division(int a, int b)
+        {
+            Console.WriteLine("Division Result = " + (a / b));
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculation Cal = new Calculation();
+
+            MulticastDelegate del = Cal.Addition;
+
+            del += Cal.Subtraction;
+            del += Cal.Multiplication;
+            del += Cal.Division;
+
+            del(23,21);
+
+        }
+    }
+}
